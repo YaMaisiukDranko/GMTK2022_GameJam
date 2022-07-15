592487e7a8b8a7dc1f0f20037ccb905f6fe8f09a
Yaroslav <yarikmeisan@yandex.com>
2022-07-16 00:29:04 +0300

    Setting up DiceSideCheck Script

---
+++ GMTK2022_GameJam/Assets/Scripts/DiceSideCheck.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSideCheck : MonoBehaviour
+{
+    private Vector3 diceVelocity;
+    public int diceNumber;
+
+    private void Start()
+    {
+        DiceScript.diceNum = diceNumber;
+    }
+
+    private void FixedUpdate()
+    {
+        diceVelocity = DiceScript.diceVelocity;
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f) //Checking Dice Side
+        {
+            switch (col.gameObject.name)
+            {
+                case "Side1":
+                    DiceScript.diceNum = 6;
+                    break;
+                case "Side2":
+                    DiceScript.diceNum = 5;
+                    break;
+                case "Side3":
+                    DiceScript.diceNum = 4;
+                    break;
+                case "Side4":
+                    DiceScript.diceNum = 3;
+                    break;
+                case "Side5":
+                    DiceScript.diceNum = 2;
+                    break;
+                case "Side6":
+                    DiceScript.diceNum = 1;
+                    break;
+            }
+        }
+    }
+}
