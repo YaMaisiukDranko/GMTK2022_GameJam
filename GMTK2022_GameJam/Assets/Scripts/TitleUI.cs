7c7eb19390fb893fdd4dfdf063439953baee6469
HAwka <v-petrischev2010@yandex.ru>
2022-07-17 02:02:27 +0300

    Menu scene is completed

--- GMTK2022_GameJam/Assets/Scripts/TitleUI.cs
+++ GMTK2022_GameJam/Assets/Scripts/TitleUI.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 
 public class TitleUI : MonoBehaviour
 {
+    private Transform secreatSpeach;
     private void Start()
     {
+        secreatSpeach = GetComponent<Transform>();
+        secreatSpeach.Find("secretSpeach").gameObject.SetActive(false);
+
         transform.Find("startBtn").GetComponent<Button>().onClick.AddListener(() =>
         {
             SceneManager.LoadScene(1);
+        });
+
+        transform.Find("quitBtn").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            secreatSpeach.Find("secretSpeach").gameObject.SetActive(true);
         });
     }
 }
