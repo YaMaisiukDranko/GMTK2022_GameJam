ea95c5f4bb8cd4ef47d8cd2b8b37fd3b0a26ba1f
Yaroslav <yarikmeisan@yandex.com>
2022-07-16 21:12:29 +0300

    Paste code to EnemyScript from Stone.cs

--- GMTK2022_GameJam/Assets/Scripts/EnemyScript.cs
+++ GMTK2022_GameJam/Assets/Scripts/EnemyScript.cs
@@ -4,5 +4,58 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    
+    public Route currentRoute;
+
+    public int routePosition;
+
+    public int steps;
+
+    private bool isMoving;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isMoving)
+        {
+            Debug.Log("FigureMoving");
+            if(!isMoving)
+            {
+                steps = DiceNumberText.diceNumber;
+                if(routePosition + steps < currentRoute.childNodeList.Count)
+                {
+                    StartCoroutine(Move());
+                }
+            }
+        }
+    }
+
+    private IEnumerator Move()
+    {
+        if(isMoving)
+        {
+            yield break;
+        }
+        isMoving = true;
+
+        while(steps > 0)
+        {
+            Vector3 nextPos = currentRoute.childNodeList[routePosition + 1].position;
+
+            while(MoveToNextNote(nextPos))
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.1f);
+            steps--;
+            routePosition++;
+        }
+
+        isMoving = false;
+    }
+
+    private bool MoveToNextNote(Vector3 goal)
+    {
+        return goal != (transform.position = Vector3.MoveTowards(transform.position, goal, 2f * Time.deltaTime));
+    }
 }
