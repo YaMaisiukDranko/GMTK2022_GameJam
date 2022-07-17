0d7266d9bd360651820ca637233c79a030321707
Yaroslav <yarikmeisan@yandex.com>
2022-07-17 13:45:21 +0300

    Setting up memeScreen

--- GMTK2022_GameJam/Assets/Scripts/MemeManager.cs
+++ GMTK2022_GameJam/Assets/Scripts/MemeManager.cs
@@ -2,32 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class MemeManager : MonoBehaviour
 {
     public Sprite[] memeSprites;
+    public Sprite currentSprite;
+    public GameObject memeScreen;
     public SpriteRenderer sr;
     public int currentStep;
     private Stone _stone;
-    private int spriteInt;
+    public int spriteInt;
 
-    private void Awake()
-    {
-        currentStep = _stone.routePosition;
-    }
+    
 
     private void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        currentSprite = sr.sprite;
+        currentStep = _stone.routePosition;
+        memeScreen = GameObject.Find("MemeSCREEN");
+        sr = memeScreen.GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         currentStep = _stone.routePosition;
+        SetMemeToScreen();
     }
 
     void SetMemeToScreen()
     {
-        sr.sprite = memeSprites[spriteInt];
+        sr.sprite = memeSprites[1];
     }
 }
