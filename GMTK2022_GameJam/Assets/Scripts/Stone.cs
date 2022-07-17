3ccc8dc550d13ac0268ff7c24e997019499c2c34
Yaroslav <yarikmeisan@yandex.com>
2022-07-17 10:10:55 +0300

    Delete 30 platforms

--- GMTK2022_GameJam/Assets/Scripts/Stone.cs
+++ GMTK2022_GameJam/Assets/Scripts/Stone.cs
@@ -52,7 +52,7 @@ public class Stone : MonoBehaviour
         while(steps > 0)
         {
             Vector3 nextPos = currentRoute.childNodeList[routePosition + 1].position;
-            int gameOverValue = 70 +- 1; //Game Over Platform
+            int gameOverValue = 40 +- 1; //Game Over Platform
             if (nextPos == currentRoute.childNodeList[gameOverValue].position)
             {
                 OnPlayerStandOnFinalRoute?.Invoke(this, EventArgs.Empty);
